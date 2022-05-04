4223581618f2da8150b04351888d991d4aa58dce
Daniel Granja <thedanielgranja@gmail.com>
2022-05-04 11:44:56 +0100

    lab 8 continuacao

--- lab8/portfolio/urls.py
+++ lab8/portfolio/urls.py
@@ -6,4 +6,8 @@ name = 'home'
 urlpatterns = [
     path('home', views.home_page_view, name='home'),
     path('', views.home_page_view),
+    path('apresentação', views.apresentacao_page_view, name='apresentação'),
+    path('formação', views.formacao_page_view, name='formação'),
+    path('projetos', views.projetos_page_view, name='projetos'),
+    path('competencias', views.competencias_page_view, name='competencias'),
 ]
--- lab8/portfolio/views.py
+++ lab8/portfolio/views.py
@@ -4,4 +4,16 @@ from django.shortcuts import render
 def home_page_view(request):
     return render(request, 'portfolio/layout.html')
 
+
 # Create your views here.
+def apresentacao_page_view(request):
+    return render(request, 'portfolio/apresentação.html')
+
+def formacao_page_view(request):
+    return render(request, 'portfolio/formação.html')
+
+def projetos_page_view(request):
+    return render(request, 'portfolio/projetos.html')
+
+def competencias_page_view(request):
+    return render(request, 'portfolio/competencias.html')
