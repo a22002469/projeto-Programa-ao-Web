30d4acad3f5ea555c048ab6604e98e7545a659c4
Daniel Granja <thedanielgranja@gmail.com>
2022-05-11 11:39:15 +0100

    Blog bd atualizada

--- lab8/portfolio/models.py
+++ lab8/portfolio/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    autor = models.CharField(max_length=50)
+    data = models.DateTimeField(auto_now_add=True)
+    titulo = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=500)
