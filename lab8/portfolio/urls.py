3460a707c2a8c1a624a1adebf9513181fa1c418c
Daniel Granja <thedanielgranja@gmail.com>
2022-05-23 16:00:39 +0100

    Layout com mais paginas

--- lab8/portfolio/urls.py
+++ lab8/portfolio/urls.py
@@ -14,6 +14,10 @@ urlpatterns = [
     path('nova/', views.view_novo_post, name='nova'),
     path('edita/<int:post_id>', views.view_editar_post, name='edita'),
     path('apaga/<int:post_id>', views.view_apagar_post, name='apaga'),
+    path('licenciatura', views.licenciatura_page_view, name='licenciatura'),
+    path('contacto', views.contacto_page_view, name='contacto'),
+    path('web', views.web_page_view, name='web'),
+
 
 
 ]
--- lab8/portfolio/views.py
+++ lab8/portfolio/views.py
@@ -14,23 +14,35 @@ def home_page_view(request):
 def apresentacao_page_view(request):
     return render(request, 'portfolio/apresentação.html')
 
+
+def licenciatura_page_view(request):
+    return render(request, 'portfolio/licenciatura.html')
+
+
 def formacao_page_view(request):
     return render(request, 'portfolio/formação.html')
+
 
 def projetos_page_view(request):
     return render(request, 'portfolio/projetos.html')
 
+
 def competencias_page_view(request):
     return render(request, 'portfolio/competencias.html')
+
+def contacto_page_view(request):
+    return render(request, 'portfolio/contacto.html')
+
+def web_page_view(request):
+    return render(request, 'portfolio/web.html')
 
 def blog_page_view(request):
     context = {'post': Post.objects.all()}
 
     return render(request, 'portfolio/blog.html', context)
 
+
 def view_novo_post(request):
-
-
     form = PostForm(request.POST or None)
 
     if form.is_valid():
@@ -42,7 +54,6 @@ def view_novo_post(request):
 
 
 def view_editar_post(request, post_id):
-
     post = Post.objects.get(id=post_id)
     form = PostForm(request.POST or None, instance=post)
 
@@ -55,7 +66,6 @@ def view_editar_post(request, post_id):
 
 
 def view_apagar_post(request, post_id):
-
     post = Post.objects.get(id=post_id)
     post.delete()
     return HttpResponseRedirect(reverse('portfolio:blog'))
