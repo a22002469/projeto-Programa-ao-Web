819143620c70e9baefd89a2a6e5cf17b573a04ad
Daniel Granja- a22002469 <79157435+a22002469@users.noreply.github.com>
2022-05-27 14:29:51 +0100

    try

--- config/settings.py
+++ config/settings.py
@@ -10,13 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-
 from pathlib import Path
+# config/settings.py
 from environs import Env
 
 env = Env()
 env.read_env()
 
+DEBUG = env.bool("DEBUG", default=False)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,37 +25,34 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
+
+# config/settings.py
+
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=False)
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
-INSTALLED_APPS = (
+INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic',  # novo
     'django.contrib.staticfiles',
-    'portfolio',
     'cloudinary_storage',
     'cloudinary',
+    'portfolio',
+    'whitenoise.runserver_nostatic',
 
-)
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': "dmno3yrmr",
-    'API_KEY': "822164628752699",
-    'API_SECRET': "_uOzs4HXllGS3YcFcKmIpK4zXak",
-}
+]
 
 MIDDLEWARE = [
-
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -62,8 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # novo
-
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -90,8 +87,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": env.dj_db_url("DATABASE_URL")
 
+    "default": env.dj_db_url("DATABASE_URL")
 }
 
 # Password validation
@@ -126,22 +123,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_URL = 'static/'
+
+# STATIC_URL = '/portfolio/static/'
+
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('portfolio/static'))]
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))  # novo
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # novo
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
-STATIC_URL = 'static/'
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_URL = '/portfolio/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [str(BASE_DIR.joinpath('portfolio/static')), str(BASE_DIR.joinpath('media'))]
-STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))  # novo
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # novo
-
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',  # finds files stored in the `STATICFILES_DIRS` setting.
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',  # finds files stored in the 'static' subdirectory of
-    # each app.
-]
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': "dmno3yrmr",
+    'API_KEY': "822164628752699",
+    'API_SECRET': "_uOzs4HXllGS3YcFcKmIpK4zXak",
+}
