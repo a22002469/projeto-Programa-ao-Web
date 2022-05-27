4923db67efc3ed23b12186489f63df0b803d9448
Daniel Granja- a22002469 <79157435+a22002469@users.noreply.github.com>
2022-05-27 15:31:59 +0100

    5

--- config/settings.py
+++ config/settings.py
@@ -138,3 +138,10 @@ CLOUDINARY_STORAGE = {
     'API_KEY': "822164628752699",
     'API_SECRET': "_uOzs4HXllGS3YcFcKmIpK4zXak",
 }
+
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',     # finds files stored in the `STATICFILES_DIRS` setting.
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder', # finds files stored in the 'static' subdirectory of
+    # each app.
+]
